f1770cc8b0fb894d6a89541457194b00d9d79ecc
TutorFx <gabriel_serejo@live.com>
2023-07-24 08:32:10 -0300

    Update card.vue

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="golden-bg text-center grid gap-3 grid-rows-[repeat(3,max-content)] items-start p-6 rounded-3xl">
-    <nuxt-img sizes="xs:64px sm:64px lg:64px" class="mx-auto" densities="x1 x2 x3 x4" :src="`/assets/icons/${data.icon}`" :alt="data.title" />
+    <nuxt-img sizes="xs:64px sm:64px lg:64px" class="mx-auto w-16" densities="x1 x2 x3 x4" :src="`/assets/icons/${data.icon}`" :alt="data.title" />
     <h3 class="text-lg px-6 font-bold">
       {{ data.title }}
     </h3>
