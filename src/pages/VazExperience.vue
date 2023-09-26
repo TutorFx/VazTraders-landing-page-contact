00d36c970917a4c47ce4a5b44c8cd799bae1d8f9
Gabriel Serejo <gabriel_serejo@live.com>
2023-09-26 14:13:36 -0300

    update

--- src/pages/VazExperience.vue
+++ src/pages/VazExperience.vue
@@ -4,5 +4,9 @@
     <xp-sections-whatis />
     <xp-sections-transform />
     <xp-sections-desires />
+    <xp-sections-whois />
+    <xp-sections-whenis />
+    <xp-sections-faq />
+    <xp-sections-footer />
   </div>
 </template>
