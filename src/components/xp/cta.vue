ae2b091268edab7c3f3624f7ce0f99f53bcee209
TutorFx <gabriel_serejo@live.com>
2023-09-27 15:33:44 -0300

    actions

--- src/components/xp/cta.vue
+++ src/components/xp/cta.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grid gap-3 max-w-md">
-    <nuxt-link class="grid" href="https://sun.eduzz.com/2091427">
-      <button class="golden-bg-xp text-black px-4 py-2 font-black rounded-2xl text-md text-center">
+    <div class="grid">
+      <button @click="useBuyExperience()" class="golden-bg-xp text-black px-4 py-2 font-black rounded-2xl text-md text-center">
         EU QUERO PROSPERAR EM <br>
         TODAS AS ÁREAS DA MINHA VIDA
       </button>
-    </nuxt-link>
+    </div>
     <div class="text-center">
       Vagas Limitadas - Compra 100% segura
     </div>
